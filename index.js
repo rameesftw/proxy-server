1462086b76d07e0302b0b6bb2c30e4959f1e3523
rameesdev <muhammadramees697@gmail.com>
2023-06-18 12:21:40 +0530

    new ver

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const axios = require("axios")
 const app = express();  
 const ytsr = require('ytsr');
 const ytdl = require('ytdl-core')
+app.use(express.static('public'))
 
 app.get("/search/:q",(req,res)=>{
   const q = req.params.q;
   
-  axios.get("https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyCdMdqblqRy4ObnC7IFI-xTz5rjO9qS0zc&type=video&q="+q)
+  axios.get("https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyD4LkDVhw9RnkgprtkWLcc6fBi5u6LO5hg&type=video&q="+q)
   .then((axios)=>{return axios.data.items.map((value=>{return {videoid:value.id.videoId,title:value.snippet.title}}))}).then((data)=>{
     const videoID = data[0].videoid;
     console.log(videoID);
