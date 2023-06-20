e33c053ff08fb68f4e9079368a344e86ec39d8ad
rameesdev <muhammadramees697@gmail.com>
2023-06-20 18:06:39 +0530

    searchbar play button and playe play button linked

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ app.get("/search/:q",(req,res)=>{
   const q = req.params.q;
   console.log( req.headers['x-forwarded-for'] || req.socket.remoteAddress )  
   axios.get("https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyD4LkDVhw9RnkgprtkWLcc6fBi5u6LO5hg&type=video&q="+q)
-  .then((axios)=>{return axios.data.items.map((value=>{return {videoid:value.id.videoId,title:value.snippet.title,thumbnail:value.snippet.thumbnails.medium}}))}).then((data)=>{
-    const videoID = data[0].videoid;
+  .then((axios)=>{return axios.data.items.map((value=>{return {videoid:value.id.videoId,title:value.snippet.title,thumbnail:value.snippet.thumbnails.medium}}))}).then((data)=>{  
     res.json(data);
     
    
