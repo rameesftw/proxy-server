4edba106e0f1a835a47a307824aa737c9386e990
rameesdev <105360057+rameesdev@users.noreply.github.com>
2024-01-23 10:05:48 +0000

    devlopment

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 git add .
 git commit -m "devlopment"
 git push origin glitch
-curl https://api.render.com/deploy/srv-ckvqtamb0mos73c13si0?key=OjgdNeazh4c
+curl https://api.render.com/deploy/srv-cmno32md3nmc739f5i0g?key=N4FfrlrW8fM
